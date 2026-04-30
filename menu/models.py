f305fd3ca430327eb85eec1acc4863785784b852
Artem <artemzahar777@knu.ua>
2026-04-30 22:06:03 +0300

    Create Category and Dish models for restaurant

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва категорії")
+    description = models.TextField(blank=True, verbose_name="Опис")
+
+    def __str__(self):
+        return self.name
+
+class Dish(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='dishes')
+    name = models.CharField(max_length=200, verbose_name="Назва страви")
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="Ціна")
+    is_vegetarian = models.BooleanField(default=False, verbose_name="Вегетаріанська")
+    is_available = models.BooleanField(default=True, verbose_name="В наявності")
+
+    def __str__(self):
+        return self.name
