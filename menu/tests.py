896eda1f10b5782a1778237ae66326e6a1d95145
Artem <artemzahar777@knu.ua>
2026-04-30 22:09:29 +0300

    Add unit tests for menu models

--- menu/tests.py
+++ menu/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Category, Dish
 
-# Create your tests here.
+class MenuTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Десерти", description="Солодощі")
+        Dish.objects.create(
+            category=self.category,
+            name="Чизкейк",
+            price=150.00,
+            is_vegetarian=True
+        )
+
+    def test_dish_creation(self):
+        dish = Dish.objects.get(name="Чизкейк")
+        self.assertEqual(dish.price, 150.00)
+        self.assertEqual(dish.category.name, "Десерти")
+        self.assertTrue(dish.is_vegetarian)
